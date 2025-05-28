d4982ef8d1a27df540fc992b579e4ac269df571e
wangjiahao <1522128093@qq.com>
2025-05-28 18:35:44 +0800

    feat: dashboard rich text

--- frontend/src/views/dashboard/components/sq-text/index.vue
+++ frontend/src/views/dashboard/components/sq-text/index.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+import SQRichText from "@/views/dashboard/components/sq-text/SQRichText.vue";
 </script>
 
 <template>
-
+  <SQRichText></SQRichText>
 </template>
 
 <style scoped lang="less">
