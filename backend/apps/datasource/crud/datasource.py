d77d1534cf5821998488565133d41b764c76a9ed
junjun <752696511@qq.com>
2025-07-24 23:08:58 +0800

    fix: table permission

--- backend/apps/datasource/crud/datasource.py
+++ backend/apps/datasource/crud/datasource.py
@@ -294,7 +294,7 @@ def preview(session: SessionDep, current_user: CurrentUser, id: int, data: Table
     elif ds.type == "sqlServer":
         sql = f"""SELECT [{"], [".join(fields)}] FROM [{conf.dbSchema}].[{data.table.table_name}]
             {where} 
-            ORDER BY [{data.fields[0].field_name}]
+            ORDER BY [{fields[0]}]
             OFFSET 0 ROWS FETCH NEXT 100 ROWS ONLY"""
     elif ds.type == "pg" or ds.type == "excel":
         sql = f"""SELECT "{'", "'.join(fields)}" FROM "{conf.dbSchema}"."{data.table.table_name}" 
@@ -303,7 +303,7 @@ def preview(session: SessionDep, current_user: CurrentUser, id: int, data: Table
     elif ds.type == "oracle":
         sql = f"""SELECT "{'", "'.join(fields)}" FROM "{conf.dbSchema}"."{data.table.table_name}"
             {where} 
-            ORDER BY "{data.fields[0].field_name}"
+            ORDER BY "{fields[0]}"
             OFFSET 0 ROWS FETCH NEXT 100 ROWS ONLY"""
     return exec_sql(ds, sql)
 
