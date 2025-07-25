31cc4f4b572519ab502f1c93f59abc1e7faedc3c
ulleo <ulleo.wang@gmail.com>
2025-07-25 16:18:34 +0800

    chore: remove print data

--- backend/apps/chat/task/llm.py
+++ backend/apps/chat/task/llm.py
@@ -834,7 +834,6 @@ class LLMService:
 
             # execute sql
             result = self.execute_sql(sql=sql)
-            print(result)
             self.save_sql_data(data_obj=result)
             if in_chat:
                 yield orjson.dumps({'content': 'execute-success', 'type': 'sql-data'}).decode() + '\n\n'
