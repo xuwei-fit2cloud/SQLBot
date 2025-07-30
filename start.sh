8f139a0a4621afe35848880ff64031960ae2921f
xuwei-fit2cloud <wei@fit2cloud.com>
2025-07-30 18:21:17 +0800

    chore: add mcp server

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@ SSR_PATH=/opt/sqlbot/g2-ssr
 APP_PATH=/opt/sqlbot/app
 nohup node $SSR_PATH/app.js &
 
+nohup uvicorn main:mcp_app --host 0.0.0.0 --port 8001 &
+
 cd $APP_PATH
 uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1 --proxy-headers
