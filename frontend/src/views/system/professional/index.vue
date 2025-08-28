88840be0c9508720da329fba1deefff9005c353b
dataeaseShu <tong.shu@fit2cloud.com>
2025-08-28 16:41:57 +0800

    fix(Professional terminology): The term description is displayed in up to three lines.

--- frontend/src/views/system/professional/index.vue
+++ frontend/src/views/system/professional/index.vue
@@ -642,9 +642,10 @@ const deleteHandlerItem = (idx: number) => {
         display: -webkit-box;
         max-height: 66px;
         -webkit-box-orient: vertical;
-        -webkit-line-clamp: 2; /* 限制行数为3 */
+        -webkit-line-clamp: 3; /* 限制行数为3 */
         overflow: hidden;
         text-overflow: ellipsis;
+        word-break: break-word;
       }
       .ed-icon {
         color: #646a73;
