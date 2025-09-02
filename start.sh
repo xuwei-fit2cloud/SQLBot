52a052e03046eadb63348824eb25b7b6514ded5f
junjun <junjie.xia@fit2cloud.com>
2025-09-02 17:27:32 +0800

    build: allinone

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 SSR_PATH=/opt/sqlbot/g2-ssr
 APP_PATH=/opt/sqlbot/app
 
-/usr/local/bin/docker-entrypoint.sh
+/usr/local/bin/docker-entrypoint.sh postgres
 sleep 5
 wait-for-it 127.0.0.1:5432 --timeout=120 --strict -- echo -e "\033[1;32mPostgreSQL started.\033[0m"
 
