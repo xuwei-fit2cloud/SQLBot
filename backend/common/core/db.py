5b324182928d40c6a18db702543202fc76043a5f
junjun <junjie.xia@fit2cloud.com>
2025-09-10 10:39:33 +0800

    refactor: Increase database connection pool

--- backend/common/core/db.py
+++ backend/common/core/db.py
@@ -1,14 +1,15 @@
 from sqlmodel import Session, create_engine, SQLModel
 
-
 from common.core.config import settings
 
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI), pool_size=20, max_overflow=30, pool_recycle=3600,
+                       pool_pre_ping=True)
 
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 def init_db():
     SQLModel.metadata.create_all(engine)
