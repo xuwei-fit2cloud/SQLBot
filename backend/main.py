c072bd2103ad9a4e6aac87722651c2d405035f81
junjun <junjie.xia@fit2cloud.com>
2025-09-22 15:55:35 +0800

    feat: MCP support get third-part system datasource

--- backend/main.py
+++ backend/main.py
@@ -73,7 +73,7 @@ mcp = FastApiMCP(
     description="SQLBot MCP Server",
     describe_all_responses=True,
     describe_full_response_schema=True,
-    include_operations=["get_datasource_list", "get_model_list", "mcp_question", "mcp_start"]
+    include_operations=["get_datasource_list", "get_model_list", "mcp_question", "mcp_start", "mcp_assistant"]
 )
 
 mcp.mount(mcp_app)
