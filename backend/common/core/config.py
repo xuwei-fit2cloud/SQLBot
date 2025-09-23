7537fb27ca9d67f93bdb4adffd698ea3dd977367
junjun <junjie.xia@fit2cloud.com>
2025-09-23 17:48:11 +0800

    refactor: add parameter control table count

--- backend/common/core/config.py
+++ backend/common/core/config.py
@@ -101,7 +101,7 @@ class Settings(BaseSettings):
     PG_POOL_RECYCLE: int = 3600
     PG_POOL_PRE_PING: bool = True
 
-    TABLE_EMBEDDING_COUNT: int = 30
+    TABLE_EMBEDDING_COUNT: int = 10
 
 
 settings = Settings()  # type: ignore
