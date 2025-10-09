1ac0b743660679de5a5652bd9ada23909ec6f15e
junjun <junjie.xia@fit2cloud.com>
2025-10-09 14:18:10 +0800

    feat: Vector retrieval matches tables

--- backend/apps/datasource/crud/table.py
+++ backend/apps/datasource/crud/table.py
@@ -38,7 +38,6 @@ def run_fill_empty_table_embedding(session: Session):
     SQLBotLogUtil.info('get tables')
     stmt = select(CoreTable.id).where(and_(CoreTable.embedding.is_(None)))
     results = session.execute(stmt).scalars().all()
-    SQLBotLogUtil.info(json.dumps(results))
 
     save_table_embedding(session, results)
 
